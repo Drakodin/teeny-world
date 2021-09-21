d2e2418a6d95a955faf57d26fc0d233200ac0465
Drakodin <talia.yl.tian@gmail.com>
2021-09-20 22:30:55 -0400

    Remove more starter code, make cloud required.

--- starter/scripts/animation.js
+++ starter/scripts/animation.js
@@ -1,14 +1,12 @@
-// DO NOT MODIFY THIS FILE
 // This file provides simple animations to elements on the screen
 
 /**
  * Applies a simple sliding animation infinitely on clouds
+ * 
+ * This code should be toggleable if we ever want to add a button
+ * to control the animation.
  * @param {HTMLElement} node The cloud element on the screen.
  */
 function animate(node) {
-    if (node.classList.contains("anim-slide")) {
-        node.classList.remove("anim-slide");
-    } else {
-        node.classList.add("anim-slide");
-    }
+
 }
